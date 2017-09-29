4fa293b4469be02e9237a658494d27b15e6dcee5
Vittorio Ballestra <vittorio.ballestra@drafintech.it>
2017-09-29 09:22:23 +0200

    signature as requested.
    Signed-off-by: Vittorio Ballestra <vittorio.ballestra@drafintech.it>

--- roman.js
+++ roman.js
@@ -4,5 +4,14 @@ var roman = function() {
 
 };
 
+roman.prototype.toInt = function() {
+
+};
+
+roman.prototype.toString = function() {
+
+};
+
+roman.prototype.constructor = roman;
 
 module.exports = roman;
--- test/roman.spec.js
+++ test/roman.spec.js
@@ -6,4 +6,16 @@ describe('roman',function(){
     it('is a function',function(){
         expect(roman).to.be.a('function');
     });
+
+    it('is a roman constructor',function(){
+        expect(new roman(0)).to.be.instanceOf(roman);
+    });
+
+    it('has an "toInt" method',function(){
+        expect(roman).to.respondsTo('toInt');
+    });
+
+    it('has an "toString" method',function(){
+        expect(roman).to.respondsTo('toString');
+    });
 });
